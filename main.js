66bbdf0773075d63dae2a65147954b549e834879
Ivan yuan <Ivan_Yuan@cmoney.com.tw>
2022-03-08 18:53:59 +0800

    修css

--- main.js
+++ main.js
@@ -9,8 +9,7 @@ function onLoad() {
 }
 
 function getData() {
-	document.querySelector('#Loading').classList.remove('js-hidden');
-	fetch('./data.json')
+	fetch('../data/data.json')
 		.then(res => res.json())
 		.then(json => {
 			document.querySelector('#Loading').classList.add('js-hidden');
@@ -18,56 +17,46 @@ function getData() {
 			totalLen = json.length;
 			data = json;
 			setbtn()
-			changeTable(i);
+			render(i);
 			switchPage();
 		})
 }
 
-function changeTable(v) {
-	let str = '';
-	const Tbody = document.querySelector('#Tbody');
-	if (perPage >= totalLen) {
-		str = setInfo(v, totalLen)
-	}
-	else {
-		str = setInfo(v, v + perPage)
-	}
-	Tbody.innerHTML = str;
+function render(v, str='') {
+	const elemSpotTableTbody = document.querySelector('#SpotTableTbody');
+	perPage >= totalLen ? str = setInfo(v, totalLen) : str = setInfo(v, v + perPage);
+	elemSpotTableTbody.innerHTML = str;
 }
 
-function setInfo(v, len) {
-	let str = '';
+function setInfo(v, len, str='') {
 	for (let i = v; i < len; i++) {
 		if (data[i]) {
-			str += `<tr>
-			<td>${i + 1}</td>
-			<td>${data[i].City}</td>
-			<td>
-			<div class="img__small">
-				<img class="img-resp" src=${data[i].PicURL} width="91" height="54">
-				<div class="img__big">
-				<img class="img-resp" src=${data[i].PicURL} width="348" height="237">
+			let desc = data[i].HostWords
+			str += `
+			${i % 2 !== 0 ? `<tr class="bg-grey spotTable__tr">`
+			:`<tr class="spotTable__tr">`}
+			<td class="text-center spotTable__td">${i + 1}</td>
+			<td class="spotTable__td">${data[i].City}</td>
+			<td class="spotTable__td">
+				<div class="spotTable__smBox">
+					<img class="spotTable__img" src=${data[i].PicURL} width="91" height="54">
+					<div class="spotTable__lgBox">
+						<img class="spotTable__img" src=${data[i].PicURL} width="348" height="237">
+					</div>
 				</div>
-			</div>
 			</td>
-			${isHas(i)}
-			<td>${data[i].HostWords.substring(0, 50)}...</td>
+			<td class="spotTable__td">
+			${data[i].Url === '' ? `${data[i].Name}` :
+			`<a class="spotTable__link" href=${data[i].Url} target="_blank">${data[i].Name}</a>`}
+			</td>
+			<td class="spotTable__td">
+			${desc.length > 50 ? `${data[i].HostWords.substring(0, 50)}...`
+			: `${data[i].HostWords}`}
+			</td>
 			</tr>`
 		}
 	}
 	return str;
-}
-
-function isHas(x) {
-	if (data[x].Url === '') {
-		return `<td>${data[x].Name}</td>`;
-	} else {
-		return `<td>
-		<a href=${data[x].Url} target="_blank">
-		${data[x].Name}
-		</a>
-		</td>`;
-	}
 }
 
 function setbtn() {
@@ -76,7 +65,7 @@ function setbtn() {
 	const page = document.querySelector('#Page'),
 		btn = page.children;
 	for (let i = 0; i < len; i++) {
-		str += `<button class="btn" data-i="${i * perPage}">${i + 1}</button>`
+		str += `<button type="button" class="page__btn" data-i="${i * perPage}">${i + 1}</button>`
 	}
 	page.innerHTML = str;
 	btn[btnNum].classList.add('js-btn');
@@ -84,14 +73,14 @@ function setbtn() {
 }
 //監聽換頁事件
 function switchPage() {
-	const btn = document.querySelectorAll('#Page .btn');
-	for (let i = 0; i < btn.length; i++) {
-		btn[i].addEventListener('click', function () {
+	const elemPageBtn = document.querySelectorAll('#Page .page__btn');
+	for (let i = 0; i < elemPageBtn.length; i++) {
+		elemPageBtn[i].addEventListener('click', function () {
 			if (btnNum === i) return;
 			let key = parseInt(this.dataset.i);
-			changeTable(key);
+			render(key);
 			this.classList.add('js-btn');
-			btn[btnNum].classList.remove('js-btn');
+			elemPageBtn[btnNum].classList.remove('js-btn');
 			btnNum = i;
 		}, false);
 	}
